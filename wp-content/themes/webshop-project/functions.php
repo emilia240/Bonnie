012e2c44f19e604aab50e1d4293e00f4ca336860
fxlks <karmar01@easv365.dk>
2025-08-28 14:51:49 +0200

    Update functions.php

--- wp-content/themes/webshop-project/functions.php
+++ wp-content/themes/webshop-project/functions.php
@@ -4,3 +4,25 @@ function wb_load_resources() {
 }
 
 add_action( 'wp_enqueue_scripts', 'wb_load_resources' );
+
+function create_blog_post_type() {
+    $labels = array(
+        'name' => 'Blog',
+        'singular_name' => 'Blog Post',
+        'menu_name' => 'Blog',
+        'all_items' => 'All Blog Posts',
+        'add_new_item' => 'Add New Blog Post',
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'menu_position' => 5,
+        'show_in_rest' => true,
+    );
+
+    register_post_type('blog', $args);
+}
+add_action('init', 'create_blog_post_type');
