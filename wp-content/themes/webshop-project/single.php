6c9ccbdac95cd953e72be35923203210fe3a4c21
Ivankirev39 <ivakir01@easv365.dk>
2025-08-29 16:40:25 +0200

    Enabled comments in single php
    Added the if statement that enables the comments in single php. Also commented out that main id just in case

--- wp-content/themes/webshop-project/single.php
+++ wp-content/themes/webshop-project/single.php
@@ -1,7 +1,27 @@
 <?php get_header(); ?>
 
-<main id="main-content">
+<!-- <main id="main-content">
 
-</main>
+</main> -->
 
+
+
+
+<?php if(have_posts()): ?>
+	<?php while(have_posts()): the_post() ?>
+
+
+
+
+
+
+
+
+
+<?php if(comments_open() || get_comments_number()): ?>
+				<?php comments_template(); ?>
+			<?php endif; ?>
+
+    <?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
