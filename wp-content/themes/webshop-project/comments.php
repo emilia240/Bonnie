4ed08d464a93dbf5071d44bd121b6a50d21d2ffa
Ivankirev39 <ivakir01@easv365.dk>
2025-08-29 16:46:29 +0200

    Updated comments php
    Added the code that prepares the file to display the comments

--- wp-content/themes/webshop-project/comments.php
+++ wp-content/themes/webshop-project/comments.php
@@ -1,7 +1,20 @@
 <div id="comments" class="comments-area">
 
 
-<!-- Comments go here  -->
+<?php if(have_comments()): ?>
+	<h2 class="comments-title">Comments</h2>
+		<ul class="commentlist">
+			<?php wp_list_comments(); ?>
+		</ul>
+<?php the_comments_navigation(); ?> 
+<?php endif; ?>
+
+<?php if(comments_open()): ?>
+	<?php comment_form(); ?>
+<?php else: ?>
+	<p class="no-comments">Comments are closed</p>
+<?php endif; ?>
+
 
 
 </div>
