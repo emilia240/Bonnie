94534957cc6af5382f573052e8cd9ffb5ee865d8
emilia240 <emisco01@easv365.dk>
2025-09-01 03:23:18 +0200

    assets; template-parts, tailwind, colors, fonts, index-hero

--- wp-content/themes/webshop-project/header.php
+++ wp-content/themes/webshop-project/header.php
@@ -1,10 +1,10 @@
-<?php?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css"> 
+  <!-- If this is in functions.php, you can remove it from here -->
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
