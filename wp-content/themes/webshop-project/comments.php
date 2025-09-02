670ddd9140780265794b2170066e70de3f9fca28
Ivankirev39 <ivakir01@easv365.dk>
2025-09-02 11:06:01 +0200

    Minor changes

--- wp-content/themes/webshop-project/comments.php
+++ wp-content/themes/webshop-project/comments.php
@@ -1,20 +1,17 @@
 <div id="comments" class="comments-area">
 
-
-<?php if(have_comments()): ?>
-	<h2 class="comments-title">Comments</h2>
-		<ul class="commentlist">
-			<?php wp_list_comments(); ?>
-		</ul>
-<?php the_comments_navigation(); ?> 
+<?php if ( comments_open() ) : ?>
+    <?php comment_form(); ?>
+<?php else : ?>
+    <p class="no-comments">Comments are closed</p>
 <?php endif; ?>
 
-<?php if(comments_open()): ?>
-	<?php comment_form(); ?>
-<?php else: ?>
-	<p class="no-comments">Comments are closed</p>
+<?php if ( have_comments() ) : ?>
+    <h2 class="comments-title">Comments</h2>
+    <ul class="commentlist">
+        <?php wp_list_comments(); ?>
+    </ul>
+    <?php the_comments_navigation(); ?> 
 <?php endif; ?>
-
-
 
 </div>
