303672ebfc985a2beba58b2ccf7b3afe69d215ab
emilia240 <emisco01@easv365.dk>
2025-09-03 20:50:36 +0200

    hover

--- wp-content/themes/webshop-project/home.php
+++ wp-content/themes/webshop-project/home.php
@@ -4,7 +4,7 @@
     <div class="flex flex-row justify-center !gap-4 !my-8 !px-4">
         <!-- ALL option -->
         <form method="get" action="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>">
-            <button type="submit" name="cat" value="" class="uppercase font-bold px-4 py-2 text-[#1D1D1D] hover:underline" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
+            <button type="submit" name="cat" value="" class="uppercase font-bold px-4 py-2 text-[#1D1D1D] hover:underline hover:text-[#1C3361]" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
                 ALL
             </button>
         </form>
@@ -14,7 +14,7 @@
         ?>
         <?php foreach ($categories as $category): ?>
             <form method="get" action="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>">
-                <button type="submit" name="cat" value="<?php echo esc_attr($category->term_id); ?>" class="uppercase px-4 py-2 text-[#1D1D1D] hover:underline" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
+                <button type="submit" name="cat" value="<?php echo esc_attr($category->term_id); ?>" class="uppercase px-4 py-2 text-[#1D1D1D] hover:underline hover:text-[#1C3361]" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
                     <?php echo esc_html($category->name); ?>
                 </button>
             </form>
@@ -35,6 +35,7 @@
             <p>No articles found.</p>
 
              <!-- Pagination -->
+              <!-- to style after we get 10+ articles; later -->
             <div class="mt-8 flex justify-center">
                 <?php echo paginate_links(array(
                     'total' => $query->max_num_pages,
