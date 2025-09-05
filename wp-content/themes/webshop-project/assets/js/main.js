81af8ddb7871233d17fa64876da3122e87f7b344
Ivankirev39 <ivakir01@easv365.dk>
2025-09-05 13:11:27 +0200

    script changes

--- wp-content/themes/webshop-project/assets/js/main.js
+++ wp-content/themes/webshop-project/assets/js/main.js
@@ -2,17 +2,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     const commentList = document.querySelector('.comment-list');
     const seeAllBtn = document.querySelector('.see-all-comments-btn');
-    
+    const comments = commentList ? commentList.querySelectorAll('li') : [];
+
+    // Hide all comments except the first
+    if (commentList && comments.length > 1) {
+        comments.forEach((comment, i) => {
+            if (i > 0) {
+                comment.classList.add('hidden-comment');
+            }
+        });
+    }
+
     if (seeAllBtn && commentList) {
         seeAllBtn.addEventListener('click', function(e) {
             e.preventDefault();
-            
+
             if (commentList.classList.contains('show-all')) {
                 commentList.classList.remove('show-all');
                 this.textContent = 'See all comments';
+                comments.forEach((comment, i) => {
+                    if (i > 0) {
+                        comment.classList.add('hidden-comment');
+                    }
+                });
             } else {
                 commentList.classList.add('show-all');
                 this.textContent = 'Show less';
+                comments.forEach((comment, i) => {
+                    if (i > 0) {
+                        comment.classList.remove('hidden-comment');
+                    }
+                });
             }
         });
     }
