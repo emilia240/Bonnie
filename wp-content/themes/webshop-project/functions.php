4fcc16d70642ee045ad60073ef06bf4c2fc5cd26
emilia240 <emisco01@easv365.dk>
2025-09-05 13:13:57 +0200

    functions js

--- wp-content/themes/webshop-project/functions.php
+++ wp-content/themes/webshop-project/functions.php
@@ -5,12 +5,14 @@ function wb_load_resources() {
     wp_enqueue_style( 'adobe-fonts', 'https://use.typekit.net/fjs8eoq.css' );
 }
 
+add_action( 'wp_enqueue_scripts', 'wb_load_resources' );
+
 function enqueue_main_js() {
     wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array(), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_main_js' );
 
-add_action( 'wp_enqueue_scripts', 'wb_load_resources' );
+
 
 /* function create_blog_post_type() {
     $labels = array(
