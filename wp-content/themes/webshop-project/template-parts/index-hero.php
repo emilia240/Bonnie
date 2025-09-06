9d35e6dd020ea43c4a2fd6d1863c078cdfc5f9bf
emilia240 <emisco01@easv365.dk>
2025-09-06 23:58:28 +0200

    ixed hero fields

--- wp-content/themes/webshop-project/template-parts/index-hero.php
+++ wp-content/themes/webshop-project/template-parts/index-hero.php
@@ -18,8 +18,7 @@
     ?>
 
     <!-- Slider container -->
-     <div id="hero-slider" class="absolute inset-0 w-full h-full">
-        <div id="hero-slider" class="relative w-full h-full">
+     <div id="hero-slider" class="absolute inset-0 w-full h-full">>
         <?php foreach ($slides as $i => $image): ?>
             <img src="<?php echo esc_url($image['url']); ?>"
                  class="slider-img absolute inset-0 w-full h-full object-cover transition-opacity duration-700 <?php echo $i === 0 ? 'opacity-100' : 'opacity-0'; ?>" />
@@ -32,21 +31,15 @@
     <!-- About us content -->
     <div class="relative z-10 flex justify-end w-full h-full items-center">
        <div class="w-full md:w-2/5 !px-6 !py-12 text-left">
-            <?php if ($heading): ?>
-                <h1 class="text-4xl md:text-5xl text-[#F4F4F4] mb-4 drop-shadow-lg" style="font-family: 'Aquavit', san-serif; font-weight: 700;">
-                    <?php echo esc_html($heading); ?>
-                </h1>
-            <?php endif; ?>
-            <?php if ($subheading): ?>
-                <h2 class="text-2xl md:text-3xl text-[#F4F4F4] mb-4 drop-shadow-lg" style="font-family: Bodoni MT, serif; font-weight: 500;">
-                    <?php echo esc_html($subheading); ?>
-                </h2>
-            <?php endif; ?>
-            <?php if ($text): ?>
-                <div class="text-lg md:text-xl text-[#F4F4F4] drop-shadow-lg" style="font-family: var(--font-body);">
-                    <?php echo esc_html($text); ?>
-                </div>
-            <?php endif; ?>
+            <h1 class="text-4xl md:text-5xl text-[#F4F4F4] !mb-4 drop-shadow-lg" style="font-family: 'Aquavit', san-serif; font-weight: 700;">
+                <?php echo esc_html($heading); ?>
+            </h1>
+            <h2 class="text-2xl md:text-3xl text-[#F4F4F4] !mb-4 drop-shadow-lg" style="font-family: Bodoni MT, serif; font-weight: 500;">
+                <?php echo esc_html($subheading); ?>
+            </h2>
+            <div class="text-lg md:text-xl text-[#F4F4F4] drop-shadow-lg" style="font-family: var(--font-body);">
+                <?php echo esc_html($text); ?>
+            </div>
         </div> 
     </div>
 
