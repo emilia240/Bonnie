34d88c039eb7bbed3df75f835fd7d05c5ec2a023
emilia240 <emisco01@easv365.dk>
2025-09-06 21:02:56 +0200

    sellers section

--- wp-content/themes/webshop-project/functions.php
+++ wp-content/themes/webshop-project/functions.php
@@ -8,37 +8,6 @@ function wb_load_resources() {
 
 add_action( 'wp_enqueue_scripts', 'wb_load_resources' );
 
-/* function enqueue_main_js() {
-    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array(), null, true );
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_main_js' ); */
-
-
-
-/* function create_blog_post_type() {
-    $labels = array(
-        'name' => 'Blog',
-        'singular_name' => 'Blog Post',
-        'menu_name' => 'Blog',
-        'all_items' => 'All Blog Posts',
-        'add_new_item' => 'Add New Blog Post',
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array('title', 'editor', 'thumbnail', 'comments'),
-        'menu_position' => 5,
-        'show_in_rest' => true,
-        'rewrite' => array('slug' => 'blog'),
-        'taxonomies' => array('category', 'post_tag'),
-    );
-
-    register_post_type('blog', $args);
-}
-add_action('init', 'create_blog_post_type'); */
-
 
 
 // Change comment form text
---
+++ wp-content/themes/webshop-project/template-parts/index-sellers.php
@@ -0,0 +1,51 @@
+<section class="w-full !py-12">
+  <!-- Header with lines on the side -->
+  <div class="flex items-center w-full !mb-6">
+    <hr class="flex-grow border-t border-[var(--color-dark)]">
+    <h2 class="!mx-6 text-3xl" style="font-family: 'Aquavit', san-serif; font-weight: 500;">
+      Our top-sellers
+    </h2>
+    <hr class="flex-grow border-t border-[var(--color-dark)]">
+  </div>
+
+  <!-- Subtitle and button row -->
+  <div class="flex items-center justify-between !mb-8 !px-4">
+    <div class="text-lg font-body max-w-2xl" style="font-family: var(--font-body);">
+      Pets and supplies that our customers love the most!
+    </div>
+    <a  href="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>"
+    class="bg-[#1C3361] text-[#F4F4F4] !px-6 !py-2 rounded-lg inline-flex items-center !gap-2 no-wrap hover:bg-white hover:text-[#1C3361] 
+            hover:border hover:border-[#1C3361] transition-colors duration-200" style="font-family: 'Bodoni MT', serif; font-weight: 500;">
+        Explore all
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <line x1="4" y1="12" x2="18" y2="12" stroke="currentColor" stroke-width="2"/>
+            <polyline points="12,6 18,12 12,18" stroke="currentColor" stroke-width="2" fill="none"/>
+        </svg>
+    </a>
+  </div>
+
+  <!-- Article cards -->
+  <div class="grid gap-10 w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 !mb-8 sm:px-10 ">
+        <?php
+        $args = array(
+            'post_type' => 'post', // TO BE CHANGED to webshop post type(I guess) later
+            'posts_per_page' => 4,
+            'orderby' => 'date',
+            'order' => 'DESC',
+        );
+        $query = new WP_Query($args);
+        ?>
+
+        <?php if ($query->have_posts()): ?>
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+                <!--Article Card -->
+                <?php get_template_part('template-parts/index-articles', 'card'); ?>
+            
+            <?php endwhile;?>
+            <?php wp_reset_postdata(); ?>
+
+            <?php else : ?>
+            <p>No products found.</p>
+        <?php endif; ?>
+  </div>
+</section>
