82c1eb0539fa90b1426b50eddfba8a40c316c58e
emilia240 <emisco01@easv365.dk>
2025-09-07 00:55:53 +0200

    services hover effects

--- wp-content/themes/webshop-project/template-parts/index-services.php
+++ wp-content/themes/webshop-project/template-parts/index-services.php
@@ -2,12 +2,10 @@
 
     <?php
     $headline = get_field('services_headline');
-    $image = get_sub_field('service_image');
-    $title = get_sub_field('service_title');
     ?>
 
   <!-- Header with lines on the side -->
-  <div class="flex items-center w-full !mb-6">
+  <div class="flex items-center w-full !mb-16">
     <hr class="flex-grow border-t border-[var(--color-dark)]">
     <h2 class="!mx-6 text-3xl" style="font-family: 'Aquavit', san-serif; font-weight: 500;">
         <?php if ($headline): ?>
@@ -21,11 +19,14 @@
   <div class="grid gap-10 w-full grid-cols-1 sm:grid-cols-3 !px-4 !sm:px-10">
     <?php if (have_rows('services_list')): ?>
       <?php while (have_rows('services_list')): the_row(); ?>
+        <?php
+          $image = get_sub_field('service_image');
+          $title = get_sub_field('service_title');
+        ?>
         <!-- Service Item -->
-        <div class="flex flex-col items-center">
-          <?php $image = get_sub_field('service_image'); ?>
+        <div class="flex flex-col items-center cursor-pointer transition-transform duration-300 hover:-translate-y-2">
           <?php if ($image): ?>
-            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($title); ?>" class="w-32 h-32 object-cover rounded-full !mb-4" />
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($title); ?>" class="w-30 h-30 object-cover rounded-full !mb-6" />
           <?php endif; ?>
           <h3 class="text-xl font-bold text-center" style="font-family: 'Aquavit', san-serif;">
             <?php echo esc_html($title); ?>
