0d6a3b02a433bcd6968b602ad8832b88f83fd542
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 00:00:10 +0200

    done with styling

--- wp-content/themes/webshop-project/template-parts/page-sustainability-goal.php
+++ wp-content/themes/webshop-project/template-parts/page-sustainability-goal.php
@@ -9,7 +9,7 @@
     ?>
 
   <!-- Header with lines on the side -->
-  <div class="flex items-center w-full !mt-10 !mb-16">
+  <div class="flex items-center w-full !mt-10 !mb-5">
     <hr class="flex-grow border-t border-[var(--color-dark)]">
     <h2 class="!mx-6 text-3xl" style="font-family: 'Aquavit', san-serif; font-weight: 500;">
         <?php if ($goalHeadline): ?>
@@ -21,36 +21,31 @@
 
 <section class="mx-auto max-w-6xl px-4 py-16 sm:py-12 ">
   <div class="mb-12 text-left">
-    <p class="mx-auto max-w-4xl text-lg">
-      <?php echo esc_html($goalIntro); ?>
+    <p class="mx-auto max-w-6xl text-xl">
+      <?php echo wp_kses_post($goalIntro); ?>
     </p>
   </div>
 
   <!-- Content Flex Container -->
   <div class="flex gap-8 items-start">
     <!-- SDG Card -->
-    <div class="flex-shrink-0 w-1/4 rounded-lg border bg-white">
+    <div class="flex-shrink-0 w-1/5 rounded-lg border bg-white">
       <div class="text-center">
         <div class="h-full w-full bg-gray-200 flex items-center justify-center">
           <img src="<?php echo esc_url($goalImage['url']); ?>" alt="">
         </div>
-        <p class="text-xl p-4"><?php echo esc_html($goalCardText); ?></p>
+        <p class="text-lg p-4"><?php echo esc_html($goalCardText); ?></p>
       </div>
     </div>
 
-    <div class="flex-1 space-y-6 text-lg">
-      <p>
-       <?php echo esc_html($goalSubText); ?>
-      </p>
+    <div class="flex-1 space-y-12 text-xl">
+      <?php echo wp_kses_post($goalSubText); ?>
     </div>
   </div>
 
-
   <div class="mt-12 text-left">
-    <p class="mx-auto max-w-4xl text-lg">
-     <?php echo esc_html($goalOutro); ?>
+    <p class="mx-auto max-w-6xl text-xl">
+     <?php echo wp_kses_post($goalOutro); ?>
     </p>
   </div>
 </section>
-
-
