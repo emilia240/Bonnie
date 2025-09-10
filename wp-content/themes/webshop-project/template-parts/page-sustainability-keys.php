a4fd4fad090e6c65ab8d68ace246a048482107cf
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 19:04:36 +0200

    1st key added, have to add the other 8 and fix the styling

--- wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
+++ wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
@@ -1,1 +1,32 @@
 <div class="keys-border"></div>
+
+<?php if( have_rows('keys') ): ?>
+  <?php while( have_rows('keys') ): the_row(); ?>
+    <div class="wrapper">
+      <!-- images column -->
+      <div>
+        <?php 
+        $keyGallery = get_sub_field('key_gallery'); 
+        if( $keyGallery ): 
+            foreach( $keyGallery as $keyGallery ): ?>
+           <img src="<?php echo esc_url($keyGallery['url']); ?>" alt="<?php echo esc_attr($keyGallery['alt']); ?>">
+            <?php endforeach; 
+        endif; ?>
+      </div>
+
+      <!-- text + icon column -->
+      <div>
+        <div class="icon-box">
+          <?php 
+          $iconBox = get_sub_field('key_icon_box'); 
+          if( $iconBox ): ?>
+            <img src="<?php echo esc_url($iconBox['url']); ?>" alt="<?php echo esc_attr($iconBox['alt']); ?>">
+          <?php endif; ?>
+        </div>
+
+        <h2><?php the_sub_field('key_title'); ?></h2>
+        <p><?php the_sub_field('key_content'); ?></p>
+      </div>
+    </div>
+  <?php endwhile; ?>
+<?php endif; ?>
