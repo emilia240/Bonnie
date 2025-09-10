5538b88731ec39bb33a531a592a1de932bec1c18
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 18:36:13 +0200

    keys section started

--- wp-content/themes/webshop-project/page-sustainability.php
+++ wp-content/themes/webshop-project/page-sustainability.php
@@ -4,6 +4,7 @@
 <?php get_template_part('template-parts/page-sustainability-hero'); ?>
 <?php get_template_part('template-parts/page-sustainability-goal'); ?>
 <?php get_template_part('template-parts/page-sustainability-initiatives'); ?>
+<?php get_template_part('template-parts/page-sustainability-keys'); ?>
 
 
 <?php get_footer(); ?>
