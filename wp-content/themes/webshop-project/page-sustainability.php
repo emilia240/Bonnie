069d10a6838a8d61f2f72bb8b61ffb37431c06c0
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 15:51:49 +0200

    initiatives section started

--- wp-content/themes/webshop-project/page-sustainability.php
+++ wp-content/themes/webshop-project/page-sustainability.php
@@ -3,6 +3,7 @@
 
 <?php get_template_part('template-parts/page-sustainability-hero'); ?>
 <?php get_template_part('template-parts/page-sustainability-goal'); ?>
+<?php get_template_part('template-parts/page-sustainability-initiatives'); ?>
 
 
 <?php get_footer(); ?>
