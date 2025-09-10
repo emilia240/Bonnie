2a1588071f78580c8d8b28223297884d257b36ae
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 19:16:14 +0200

    row-reverse works, ascending order set, styling not finished yet

--- wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
+++ wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
@@ -1,32 +1,38 @@
 <div class="keys-border"></div>
 
-<?php if( have_rows('keys') ): ?>
-  <?php while( have_rows('keys') ): the_row(); ?>
-    <div class="wrapper">
-      <!-- images column -->
-      <div>
-        <?php 
-        $keyGallery = get_sub_field('key_gallery'); 
-        if( $keyGallery ): 
-            foreach( $keyGallery as $keyGallery ): ?>
-           <img src="<?php echo esc_url($keyGallery['url']); ?>" alt="<?php echo esc_attr($keyGallery['alt']); ?>">
-            <?php endforeach; 
-        endif; ?>
-      </div>
+<?php
+$keys = get_field('keys');
+if ($keys) {
+    // Sort by key_title ascending
+    usort($keys, function($a, $b) {
+        return strcmp($a['key_title'], $b['key_title']);
+    });
 
-      <!-- text + icon column -->
-      <div>
-        <div class="icon-box">
-          <?php 
-          $iconBox = get_sub_field('key_icon_box'); 
-          if( $iconBox ): ?>
-            <img src="<?php echo esc_url($iconBox['url']); ?>" alt="<?php echo esc_attr($iconBox['alt']); ?>">
-          <?php endif; ?>
+    foreach ($keys as $row) : ?>
+        <div class="wrapper">
+          <!-- images column -->
+          <div>
+            <?php 
+            if( $row['key_gallery'] ): 
+                foreach( $row['key_gallery'] as $keyGallery ): ?>
+               <img src="<?php echo esc_url($keyGallery['url']); ?>" alt="<?php echo esc_attr($keyGallery['alt']); ?>">
+                <?php endforeach; 
+            endif; ?>
+          </div>
+
+          <!-- text + icon column -->
+          <div>
+            <div class="icon-box">
+              <?php 
+              $iconBox = $row['key_icon_box'];
+              if( $iconBox ): ?>
+                <img src="<?php echo esc_url($iconBox['url']); ?>" alt="<?php echo esc_attr($iconBox['alt']); ?>">
+              <?php endif; ?>
+            </div>
+
+            <h2><?php echo esc_html($row['key_title']); ?></h2>
+            <p><?php echo esc_html($row['key_content']); ?></p>
+          </div>
         </div>
-
-        <h2><?php the_sub_field('key_title'); ?></h2>
-        <p><?php the_sub_field('key_content'); ?></p>
-      </div>
-    </div>
-  <?php endwhile; ?>
-<?php endif; ?>
+    <?php endforeach;
+}
