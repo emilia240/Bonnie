93cd96265ecf3141be81cd1c1a1adee773c31804
Ivankirev39 <ivakir01@easv365.dk>
2025-09-10 21:00:01 +0200

    "our promise" section done

--- wp-content/themes/webshop-project/page-sustainability.php
+++ wp-content/themes/webshop-project/page-sustainability.php
@@ -5,6 +5,7 @@
 <?php get_template_part('template-parts/page-sustainability-goal'); ?>
 <?php get_template_part('template-parts/page-sustainability-initiatives'); ?>
 <?php get_template_part('template-parts/page-sustainability-keys'); ?>
+<?php get_template_part('template-parts/page-sustainability-promise'); ?>
 
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/webshop-project/template-parts/page-sustainability-promise.php
@@ -0,0 +1,23 @@
+<?php
+    $promiseHeadline = get_field('promise_headline');
+    $promiseText = get_field('promise_text');
+    ?>
+
+  <!-- Header with lines on the side -->
+  <div class="flex items-center w-full !mt-10 !mb-5">
+    <hr class="flex-grow border-t border-[var(--color-dark)]">
+    <h2 class="!mx-6 text-3xl" style="font-family: 'Aquavit', san-serif; font-weight: 500;">
+        <?php if ($promiseHeadline): ?>
+            <?php echo esc_html($promiseHeadline); ?>
+        <?php endif; ?>
+    </h2>
+    <hr class="flex-grow border-t border-[var(--color-dark)]">
+  </div>
+
+
+  <div class="max-w-5xl mx-auto bg-gray-200 rounded-xl p-14">
+  <p class="text-lg">
+   <?php echo esc_html($promiseText); ?>
+  </p>
+</div>
+
