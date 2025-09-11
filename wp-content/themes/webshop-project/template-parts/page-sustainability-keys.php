7c2c8f5007b508cf4d666ebb2762059b56a7594d
Ivankirev39 <ivakir01@easv365.dk>
2025-09-11 11:08:38 +0200

    fixed most of this part still not done

--- wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
+++ wp-content/themes/webshop-project/template-parts/page-sustainability-keys.php
@@ -1,60 +1,46 @@
 <div class="keys-border"></div>
 
-<?php
 
-$keys = get_field('keys');
-if ($keys) {
-    // Sort by key_title ascending
-    usort($keys, function($a, $b) {
-        return strcmp($a['key_title'], $b['key_title']);
-    });
+<?php if( have_rows('keys') ): ?>
+  <div class="keys">
+    <?php 
+    // Get all rows as an array
+    $rows = get_field('keys'); 
 
-    foreach ($keys as $index => $row) : 
-        // Maintainability: Consider moving gallery and content containers to separate template parts if reused elsewhere
-        $icon_box_class = ($index === 1) ? 'icon-box-light' : '';
-        ?>
-        <div class="wrapper" data-section="<?php echo esc_attr($index + 1); ?>">
-          <!-- images column -->
-          <div class="gallery-container">
-            <?php 
-            // Display video if key_video field exists
-            if (!empty($row['key_video'])): ?>
-              <div class="gallery-item">
-                <video controls width="100%">
-                  <source src="<?php echo esc_url($row['key_video']); ?>" type="video/mp4">
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-            <?php endif; ?>
-            <?php 
-            if( !empty($row['key_gallery']) ):
-                foreach( $row['key_gallery'] as $keyGallery ):
-                    ?>
-                    <div class="gallery-item">
-                        <img src="<?php echo esc_url($keyGallery['url']); ?>" alt="<?php echo esc_attr($keyGallery['alt']); ?>">
-                    </div>
-                    <?php
-                endforeach;
-            endif; ?>
-          </div>
-
-          <!-- text + icon column -->
-          <div class="content-container">
-            <div class="title-with-icon">
-              <div class="icon-box <?php echo esc_attr($icon_box_class); ?>">
-                <?php 
-                $iconBox = $row['key_icon_box'];
-                if( $iconBox ):
-                  // Security: Ensure alt and url are properly escaped
-                  ?>
-                  <img src="<?php echo esc_url($iconBox['url']); ?>" alt="<?php echo esc_attr($iconBox['alt']); ?>">
+    if( $rows ) :
+      foreach( array_reverse($rows) as $row ): ?>
+        <div class="key">
+          <div class="key_text">
+            <div class="key_header">
+              <div class="key_icon_box">
+                <?php if( $row['key_icon_box'] ): ?>
+                  <img src="<?php echo esc_url($row['key_icon_box']['url']); ?>" alt="<?php echo esc_attr($row['key_icon_box']['alt']); ?>">
                 <?php endif; ?>
               </div>
-              <h3><?php echo esc_html($row['key_title']); ?></h3>
+              <div class="key_title">
+                <?php echo esc_html($row['key_title']); ?>
+              </div>
             </div>
-            <p><?php echo wpautop(wp_kses_post($row['key_content'])); ?></p>
+            <div class="key_content">
+              <?php echo wpautop( wp_kses_post( $row['key_content'] ) ); ?>
+            </div>
+          </div>
+          <div class="key_images">
+            <?php if( $row['key_gallery'] ): 
+              foreach( $row['key_gallery'] as $image ): ?>
+                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+            <?php endforeach; endif; ?>
+            
+            <?php if( $row['key_video'] ): ?>
+              <div class="key_video">
+                <iframe src="<?php echo esc_url($row['key_video']); ?>" frameborder="0"></iframe>
+              </div>
+            <?php endif; ?>
           </div>
         </div>
-    <?php endforeach;
-}
-?>
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>
+
+
