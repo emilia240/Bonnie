9bf848f4de4fc7cbd8b259b381b581c0731eb346
emilia240 <emisco01@easv365.dk>
2025-09-17 23:46:19 +0200

    fixed-semantics for Page Structure part 2

--- wp-content/themes/webshop-project/home.php
+++ wp-content/themes/webshop-project/home.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
     <!-- categories filter -->
-    <div class="flex flex-row justify-center !gap-4 !my-8 !px-4">
+    <section class="flex flex-row justify-center !gap-4 !my-8 !px-4">
         <!-- ALL option -->
         <form method="get" action="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>">
             <button type="submit" name="cat" value="" class="uppercase font-bold px-4 py-2 text-[#1D1D1D] hover:underline hover:text-[#1C3361]" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
-                ALL
+                <p>ALL</p>
             </button>
         </form>
 
@@ -15,14 +15,14 @@
         <?php foreach ($categories as $category): ?>
             <form method="get" action="<?php echo esc_url(get_permalink(get_option('page_for_posts'))); ?>">
                 <button type="submit" name="cat" value="<?php echo esc_attr($category->term_id); ?>" class="uppercase px-4 py-2 text-[#1D1D1D] hover:underline hover:text-[#1C3361]" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
-                    <?php echo esc_html($category->name); ?>
+                    <p><?php echo esc_html($category->name); ?></p>
                 </button>
             </form>
         <?php endforeach; ?>
-    </div>
+    </section>
 
 <!-- Article cards -->
-  <div class="grid gap-10 w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-8 sm:px-10 ">
+    <section class="grid gap-10 w-full grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-8 sm:px-10 ">
         <?php if (have_posts()): ?>
             <?php while (have_posts()): the_post(); ?>
                     <!--Article Card -->
@@ -36,14 +36,12 @@
 
              
         <?php endif; ?>
-    </div>
+    </section>
+
     <!-- Pagination -->
-        <!-- to style after we get 10+ articles; later -->
+        <!-- to style later -->
     <div class="mt-8 flex justify-center">
-        <?php echo paginate_links(array(
-           /*  'total' => $query->max_num_pages,
-            'current' => get_query_var('paged') ? get_query_var('paged') : 1, */
-        )); ?>
+        <?php echo paginate_links(); ?>
     </div>
 
 
