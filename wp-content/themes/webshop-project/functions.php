8b0f834bc26cee23647b4b2dc59a103d21b03a52
emilia240 <emisco01@easv365.dk>
2025-09-20 20:05:53 +0200

    perf(scripts): Minify js in functions.php to improve performance

--- wp-content/themes/webshop-project/functions.php
+++ wp-content/themes/webshop-project/functions.php
@@ -14,7 +14,7 @@ function wb_load_resources() {
     wp_enqueue_style( 'style', get_template_directory_uri() . "/assets/css/style.css" );
     wp_enqueue_script( 'tailwind', 'https://cdn.tailwindcss.com', array(), null, true );
     wp_enqueue_style( 'adobe-fonts', 'https://use.typekit.net/fjs8eoq.css' );
-    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array(), null, true );
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true );
 }
 
 add_action( 'wp_enqueue_scripts', 'wb_load_resources' );
