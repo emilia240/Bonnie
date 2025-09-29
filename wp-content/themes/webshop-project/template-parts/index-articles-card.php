3143cf7a1096cf400831424dbdec61cd454a8744
emilia240 <emisco01@easv365.dk>
2025-09-29 17:49:02 +0200

    perf(style): Fix HTML errors nr 75, 76, 77

--- wp-content/themes/webshop-project/template-parts/index-articles-card.php
+++ wp-content/themes/webshop-project/template-parts/index-articles-card.php
@@ -35,7 +35,7 @@
     <div class="flex flex-col flex-grow !py-2 !px-6 text-[#1D1D1D]">
 
         <!-- Category & Tag -->
-        <div class="flex justify-between !gap-4 !py-2" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;">
+        <div class="flex justify-between !gap-4 !py-2" style="font-family: 'Skolar Sans', sans-serif; font-weight: 400;">
             <div class="category flex flex-wrap uppercase !gap-2 lg:hover:underline lg:hover:text-[#1C3361] transition-colors duration-200">
                 <?php if (!empty($categories)): ?>
                     <?php foreach ($categories as $category): ?>
@@ -63,11 +63,11 @@
         <h3 class="!mb-2 text-responsive-md" style="font-family: 'Aquavit', san-serif; font-weight: 500;"><?php echo esc_html($title); ?></h3>
         
         <!-- Description -->
-        <p class="!mb-4 text-responsive-xs line-clamp-3 sm:line-clamp-4 lg:line-clamp-5" style="font-family: 'Skolar Sans', sans-serif; font weight: 400;"><?php echo esc_html($description); ?></p>
+        <p class="!mb-4 text-responsive-xs line-clamp-3 sm:line-clamp-4 lg:line-clamp-5" style="font-family: 'Skolar Sans', sans-serif; font-weight: 400;"><?php echo esc_html($description); ?></p>
 
         <!-- Button -->
         <a href="<?php echo esc_url($url); ?>" class="inline-flex items-center !gap-2 bg-[#1C3361] text-[#F4F4F4] !px-4 !py-2 rounded-full !mt-auto w-fit lg:hover:bg-white lg:hover:text-[#1C3361] 
-        lg:hover:border lg:hover:border-[#1C3361] transition-colors duration-200" style="font-family:'Skolar Sans', sans-serif; font weight: 400;">
+        lg:hover:border lg:hover:border-[#1C3361] transition-colors duration-200" style="font-family: 'Skolar Sans', sans-serif; font-weight: 400;">
 
             <p class="text-responsive-sm"><?php echo esc_html(pll_e('Read article')); ?></p>
 
