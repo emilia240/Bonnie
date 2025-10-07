4b4fdac6c5b8a3658e39b7622ede7bbfb01f1043
fxlks <karmar01@easv365.dk>
2025-10-07 07:39:10 +0200

    fixed bg and hover in tag.php

--- wp-content/themes/webshop-project/tag.php
+++ wp-content/themes/webshop-project/tag.php
@@ -8,11 +8,12 @@
   <?php if (have_posts()) : ?>
     <ul class="space-y-8">
       <?php while (have_posts()) : the_post(); ?>
-        <li class="bg-[#1C3361] text-[#F4F4F4] rounded-lg shadow lg:hover:shadow-lg transition-shadow duration-200 transition-transform duration-300 lg:hover:scale-105 !p-8">
-          <a href="<?php the_permalink(); ?>" style="font-family: 'Aquavit', sans-serif;">
+        <li class="bg-[#1C3361] text-[#F4F4F4] rounded-lg shadow lg:hover:bg-white lg:hover:text-[#1C3361] lg:hover:border lg:hover:border-[#1C3361] transition-colors duration-200 !p-8">
+    
+         <a href="<?php the_permalink(); ?>" style="font-family: 'Aquavit', sans-serif;">
             <h2 class="text-responsive-md"><?php the_title(); ?></h2>
-          </a>
-          <p class="text-responsive-sm !mt-4" style="font-family: 'Skolar Sans', sans-serif;"><?php echo esc_html(get_the_excerpt()); ?></p>
+         </a>
+         <p class="text-responsive-sm !mt-4" style="font-family: 'Skolar Sans', sans-serif;"><?php echo esc_html(get_the_excerpt()); ?></p>
         </li>
       <?php endwhile; ?>
     </ul>
