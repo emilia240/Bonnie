1ef914bfb56397d60d9f6f6d3b37672ac37cbec2
emilia240 <emisco01@easv365.dk>
2025-10-13 23:05:17 +0200

    feat(plugin): Wordfence loging security; Plus user Stitch setup

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1760544448;s:7:"authKey";s:64:")KH&bK [^.NJninb7B4~cZ@Z|z%;UH[Z-4A*!HlRqRNmW*7p^$#&goS{+[=%k|U=";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:837;}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1760544448;s:7:"authKey";s:64:")KH&bK [^.NJninb7B4~cZ@Z|z%;UH[Z-4A*!HlRqRNmW*7p^$#&goS{+[=%k|U=";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:837;}
